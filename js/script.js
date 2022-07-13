bde0e8f217a1b1ff948df5b75c899258d323659b
Ekimov  Denis <ekimoffdenis@gmail.com>
2022-07-13 20:27:41 +0300

    v1.6 add script burger-menu

--- js/script.js
+++ js/script.js
@@ -2,9 +2,18 @@
 const burger = document?.querySelector('[data-burger]');
 const nav = document?.querySelector('[data-nav]');
 const body = document.body;
+const navItems = nav?.querySelectorAll('a')
 
 burger?.addEventListener('click', () => {
   body.classList.toggle('stop-scroll');
   burger?.classList.toggle('header__burger--active');
   nav.classList.toggle('header__menu--visible');
 });
+
+navItems.forEach(el => [
+  el.addEventListener('click', () => {
+    body.classList.remove('stop-scroll');
+    burger?.classList.remove('header__burger--active');
+    nav.classList.remove('header__menu--visible');
+  })
+])
